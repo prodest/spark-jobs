7cb62edb637dfe2b2cc3727d1067df42c1257b91
claytonsilva <clayton@xdevel.com.br>
2016-06-16 18:37:31 -0300

    ajuste na data para enviar com 'Z' respeitando o iso date

--- src/main/scala-2.11/br/gov/es/prodest/spark/Utils.scala
+++ src/main/scala-2.11/br/gov/es/prodest/spark/Utils.scala
@@ -16,7 +16,7 @@ object Utils {
   val fDateString = (value: Any, timezone: String) => value match {
     case null => null
     case s:Timestamp => {
-      val dataf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+      val dataf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
       dataf.setTimeZone(TimeZone.getTimeZone(timezone))
       dataf.format(value)
     }
