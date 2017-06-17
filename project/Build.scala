290698d3a68ca612815a30ac51f8a11b44b1b406
claytonsilva <clayton@xdevel.com.br>
2017-06-17 05:45:24 -0300

    finalizada a build 7

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object xBuild extends Build {
 
 
   lazy val buildSettings = Seq(
-    version := "0.3-build6",
+    version := "0.3-build7",
     organization := "es.gov.prodest",
     scalaVersion := "2.11.8"
   )
